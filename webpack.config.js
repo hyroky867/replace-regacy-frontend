38e2dc9701efff0398d03c30b64704e8e2b61472
hyroky867 <handm755@gmail.com>
2021-04-04 16:15:15 +0900

    chore: vue webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const { VueLoaderPlugin } = require('vue-loader-v16');
+
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
   entry: './js/script.ts',
   resolve: {
-    extensions: ['.ts', '.js'],
+    extensions: ['.ts', '.js', 'vue'],
   },
   module: {
     rules: [
@@ -11,8 +13,18 @@ module.exports = {
         test: /\.ts$/,
         use: {
           loader: 'ts-loader',
+          options: {
+            appendTsSuffixTo: [/\.vue$/],
+          },
+        },
+      },
+      {
+        test: /\.vue$/,
+        use: {
+          loader: 'vue-loader-v16',
         },
       },
     ],
   },
+  plugins: [new VueLoaderPlugin()],
 };
